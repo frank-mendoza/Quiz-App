69ea67ea82c974796411f9e043fb379dd1240627
Frank Mendoza <frnkmendoza101@gmail.com>
2023-01-11 07:47:37 +0800

    added separate component for home

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,21 +1,27 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
+import logo from '../images/zeniark-logo.png'
 
 export default function HomePage() {
   return (
     <div className="page page-home">
-      <div className="page-title">
-        <h1>Welcome to the Trivia Challenge!</h1>
-      </div>
-      <div className="page-body">
-        <p>You will be presented with 10 True or False questions.</p>
-        <p>Can you score 10/10?</p>
-      </div>
-      <div className="page-control">
-        <button>
-          <Link to="quiz">LET’S START!</Link>
-        </button>
+      <div className="page-container">
+        <div>
+          <img src={logo} alt=''/>
+        </div>
+        <div className="page-title">
+          <h1>Welcome to the Trivia Challenge!</h1>
+          <p>You will be presented with 10 True or False questions.</p>
+        </div>
+        <div className="page-body">
+          <p>Can you score 10/10?</p>
+        </div>
+        <div className="page-control">
+          <button>
+            <Link to="quiz">LET’S START!</Link>
+          </button>
+        </div>
       </div>
     </div>
-  )
+  );
 }
